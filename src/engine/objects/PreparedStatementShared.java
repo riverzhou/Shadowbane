c8e75e2cb97f34189f9e01e264d5dd2b9c1d3d23
River Zhou <riverzhou2000@163.com>
2025-07-21 19:14:07 +0800

    Update for JDK 11 LTS

--- src/engine/objects/PreparedStatementShared.java
+++ src/engine/objects/PreparedStatementShared.java
@@ -939,6 +939,7 @@ public class PreparedStatementShared implements PreparedStatement {
      *
      * @deprecated
      **/
+    @deprecated
     @Override
     public void close() throws SQLException {
         this.release(); // redirect to release method
